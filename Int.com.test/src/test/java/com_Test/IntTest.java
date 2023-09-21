3dd1742abc0ca8724f9514275f15c5c0184e2ba0
dhineshkumarepam <144765665+dhineshkumarepam@users.noreply.github.com>
2023-09-21 13:31:26 +0530

    first commit

---
+++ Int.com.test/src/test/java/com_Test/IntTest.java
@@ -0,0 +1,13 @@
+package com_Test;
+
+public class IntTest {
+	
+	private void Testing() {
+		System.out.println("Do nothing");
+	}
+	
+	private void testDo() {
+		System.out.println("can do all things");
+	}
+
+}
